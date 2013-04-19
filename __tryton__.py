2256c28411e3266dd8ddf7d158a2fa00a7626c0f
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-04-19 17:05:21 +0530

    Add new USPS shipment methods #1193 #1132
    * Renamed ParcelPost
    * Added support for FCPIS
    * Bump version to 2.0.0.3

--- __tryton__.py
+++ __tryton__.py
@@ -8,7 +8,7 @@
         The Endicia Label Server produces an integrated label image, 
         complete with Stealth (hidden) postage, return addresses, 
         verified delivery addresses, and service barcodes ''',
-    'version': '2.0.0.2',
+    'version': '2.0.0.3',
     'author': 'Openlabs Technologies & Consulting (P) LTD',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
