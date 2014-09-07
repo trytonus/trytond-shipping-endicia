bc63349296d84535339696dbb2e2d78f3adbe1e8
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-09-06 20:35:35 -0400

    Revert "Weight should be in Decimal"

--- sale.py
+++ sale.py
@@ -301,7 +301,7 @@ class SaleLine:
         ProductUom = Pool().get('product.uom')
 
         if self.product.type == 'service' or self.quantity <= 0:
-            return Decimal('0')
+            return 0
 
         if not self.product.weight:
             self.raise_user_error(
