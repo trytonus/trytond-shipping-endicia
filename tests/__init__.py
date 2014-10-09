77a3d2dfded0e1a714c9c8347813ea62b3bd7466
VaticanCameos <chakrabortypritish@gmail.com>
2014-10-09 11:58:51 +0530

    Removed BaseTestCase from __init__.py. BaseTestCase is needed to workout faulty inheritance. Changed the endicia readonly test. - assertRaises has been parameterized. test_carrier and test_stock added and refactored to increase coverage.

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,6 +9,8 @@ import unittest
 
 import trytond.tests.test_tryton
 from test_endicia import TestUSPSEndicia
+from test_carrier import CarrierTestCase
+from test_stock import ShipmentTestCase
 
 
 def suite():
@@ -18,6 +20,8 @@ def suite():
     test_suite = trytond.tests.test_tryton.suite()
     test_suite.addTests([
         unittest.TestLoader().loadTestsFromTestCase(TestUSPSEndicia),
+        unittest.TestLoader().loadTestsFromTestCase(ShipmentTestCase),
+        unittest.TestLoader().loadTestsFromTestCase(CarrierTestCase)
     ])
     return test_suite
 
